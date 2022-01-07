7dc0155d4be9bdf7771202e5a553eac5f4c55909
mosesbalaji <moses.balaji-external@tcs.com>
2022-01-07 15:55:34 +0800

    List repo

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const fs = require('fs')
   , path = require('path')
   , core = require('@actions/core')
+  , github = require('@actions/github')
   , io = require('@actions/io')
   , json2csv = require('json2csv')
 ;
 
 async function run() {
-   console.log('hello, world!')
+   const GITHUB_TOKEN = core.getInput('token');
+   const octokit = github.getOctokit('token');
+
+   const { context = {} } = github;
+   const { pull_request } = context.payload;
+
+   await octokit.rest.issues.createComment({
+     ...context.repo,
+     issue_number: pull_request.number,
+     body: 'thanks you for submmiting a pull request'
+  });
 }
 run();
